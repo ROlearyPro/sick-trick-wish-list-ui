1fe3dd5335dfcb122ab03f95229846647aaf94aa
ROlearyPro <rolearyprogramming@gmail.com>
2024-09-09 09:24:15 -0600

    getting tricks.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,22 @@
 import './App.css';
 import React from 'react';
 
-
-
+function getTrickList() {
+  fetch('http://localhost:3001/api/v1/tricks')
+    .then(res => {
+      if(!res.ok) {
+        const err = new Error(res.statusText)
+        err.statusCode = res.status
+        throw err
+      }
+      return res.json()
+    })
+    .then(data => {
+      console.log(data)
+      
+    })
+};
+console.log(getTrickList())
 function App() {
   return (
     <div className="App">
